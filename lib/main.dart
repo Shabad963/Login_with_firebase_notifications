992c73741af9b4a92e8411c7104004f5c48e1e1a
Shabad963 <shabad963@gmail.com>
2023-05-12 21:00:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       scaffoldMessengerKey: Utils.messengerKey,
       debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
-      title: 'Firebase app',
+      title: 'Firebase app with notifications',
       home: MainPage(),
     );
   }
